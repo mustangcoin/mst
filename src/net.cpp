028568544b75925e9a2f473eab076181c07ebb5a
mustang coin <mustangcoin@gmail.com>
2016-09-05 18:59:15 -0700

    updates
    updates

--- src/net.cpp
+++ src/net.cpp
@@ -1139,7 +1139,7 @@ void MapPort()
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
-    	{"216.189.153.5", "216.189.153.5"},
+    	{"node.mustangcoin.xyz", "node.mustangcoin.xyz"},
 	
 };
 
