034e684676fed908b506dbeec722f33bcc4df82c
Clients <piblockchain@gmail.com>
2017-01-29 10:57:58 -0800

    Update net.cpp
    nodes updated

--- src/net.cpp
+++ src/net.cpp
@@ -1139,7 +1139,8 @@ void MapPort()
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
-    	{"node.mustangcoin.xyz", "node.mustangcoin.xyz"},
+   	{"185.122.59.164", "185.122.59.164"},
+	{"216.189.153.5", "216.189.153.5"},
 	
 };
 
