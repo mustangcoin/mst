320748dcfd5b375f9028577903126443981897b1
cyberpay <cyberpay@users.noreply.github.com>
2017-02-20 17:51:43 -0800

    Update main.h

--- src/main.h
+++ src/main.h
@@ -64,6 +64,7 @@ extern std::map<uint256, CBlockIndex*> mapBlockIndex;
 extern std::set<std::pair<COutPoint, unsigned int> > setStakeSeen;
 extern CBlockIndex* pindexGenesisBlock;
 extern unsigned int nStakeMinAge;
+extern unsigned int nTargetSpacing;
 extern unsigned int nNodeLifespan;
 extern int nCoinbaseMaturity;
 extern int nBestHeight;
